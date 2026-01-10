f1f091c16469816ed74b160f7471c607e3439dc7
brianthegaucho <brian.a.starr@gmail.com>
2026-01-10 15:03:11 -0500

    "2: finalized bookbot"

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
+import sys
 from stats import *
 
 def main():
-	book_path = "books/frankenstein.txt"
-	text = get_book_text(book_path)
-	num_words = get_num_words(text)
-	chars_dict = get_char_dict(text)
-	list_dicts = []
-	print("============ BOOKBOT ============")
-	print("Analyzing book found at books/frankenstein.txt...")
-	print("----------- Word Count ----------")
-	print("Found",num_words,"total words")
-	print("--------- Character Count -------")
-	for char in chars_dict:
-		list_dicts.append({"char": char, "num": chars_dict[char]})
-	list_dicts.sort(reverse=True, key=sort_on)
-	for item in list_dicts:
-		ch = item["char"]
-		count = item["num"]
-		if ch.isalpha():
-			print(f"{ch}: {count}")
+	if len(sys.argv) != 2:
+		print("Usage: python3 main.py <path_to_book>")
+		sys.exit(1)
+	else:
+		book_path = str(sys.argv[1])
+		text = get_book_text(book_path)
+		num_words = get_num_words(text)
+		chars_dict = get_char_dict(text)
+		list_dicts = []
+		print("============ BOOKBOT ============")
+		print("Analyzing book found at", sys.argv[1],"...")
+		print("----------- Word Count ----------")
+		print("Found",num_words,"total words")
+		print("--------- Character Count -------")
+		for char in chars_dict:
+			list_dicts.append({"char": char, "num": chars_dict[char]})
+		list_dicts.sort(reverse=True, key=sort_on)
+		for item in list_dicts:
+			ch = item["char"]
+			count = item["num"]
+			if ch.isalpha():
+				print(f"{ch}: {count}")
 
 
 def get_book_text(path):
--- test.py
+++ test.py
@@ -1,20 +1,3 @@
-def get_book_text(path):
-	with open(path) as f:
-		return f.read()
+import sys
 
-def sort_on(items):
-	return items[0]
-
-def get_char_dict(text):
-	book_path = "books/frankenstein.txt"
-	text = get_book_text(book_path)
-	chars = {}
-	for c in text:
-		lowered = c.lower()
-		if lowered in chars:
-			chars[lowered] += 1
-		else:
-			chars[lowered] = 1
-	print(chars(sorted(key=sort_on, reverse=True)))
-
-get_char_dict("books/frankenstein.txt")
+print(sys.argv)
